ad8624ced490e3745a2cdb2d058c752dcb98a398
Vyacheslav Chernyshoff <Chernyshov.V.V@sbertech.ru>
2025-03-03 10:43:45 +0300

    changes

--- src/test/kotlin/ru/xpendence/jpa/repository/DishRepositoryTest.kt
+++ src/test/kotlin/ru/xpendence/jpa/repository/DishRepositoryTest.kt
@@ -1,11 +1,12 @@
 package ru.xpendence.jpa.repository
 
+import org.apache.commons.lang3.RandomStringUtils
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.transaction.annotation.Transactional
 import ru.xpendence.jpa.JpaApplicationTests
 import java.util.UUID
-import kotlin.random.Random
 import kotlin.test.assertEquals
 
 class DishRepositoryTest : JpaApplicationTests() {
@@ -14,14 +15,14 @@ class DishRepositoryTest : JpaApplicationTests() {
     private lateinit var repository: DishRepository
 
     @Test
-//    @Transactional
-    @DisplayName("update(): 3 селекта + 1 update, обновилось version")
+    @Transactional
+    @DisplayName("update(): неявное поведение: upload то работает, то нет")
     fun update() {
         //given
         val id = UUID.fromString("2f3cedce-ad2d-4782-98c6-c0f62b8a3f5c")
         val dish = repository.findById(id)
             .orElseThrow { IllegalStateException("Dish not found by id: $id") }
-        val newName = "Мясо по-бразильски + ${Random.nextInt(10)}"
+        val newName = "Мясо по-бразильски + ${RandomStringUtils.secure().nextAlphanumeric(32)}"
         dish?.name = newName
         //when
         repository.save(dish)
